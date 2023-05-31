f17a9b425b38824403bce27da862b7072314eb2e
Marty Neal <marty.neal@interprose.com>
2023-05-30 21:44:06 -0700

    try with workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 
-const config = {
+const dev = process.argv.includes('dev');
+
+/** @type {import('@sveltejs/kit').Config} */
+export const config = {
 	kit: {
 		adapter: adapter({
-			fallback: 'index.html'
-		})
+			pages: 'build/',
+		}),
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH,
+		}
 	}
 };
-
-export default config;
