891b198f54b97f6d5b023c9f681d91a7d56f95fc
Marty Neal <marty.neal@interprose.com>
2023-05-30 21:14:21 -0700

    gh-pages first commit

---
+++ _app/immutable/nodes/2.f84b15e6.js
@@ -0,0 +1,17 @@
+import{S as $,i as ee,s as te,k as i,q as l,a as G,l as s,m as n,r,h as t,c as U,n as z,b as h,G as a,H as D}from"../chunks/index.b64552f0.js";function ae(Z){let p,x,I,d,P,w,S,W,E,m,A,v,H,c,V,u,C,f,y,L,_,j,q,b,B,k,K;return{c(){p=i("h1"),x=l("Welcome to SvelteKit"),I=G(),d=i("p"),P=l("Visit "),w=i("a"),S=l("kit.svelte.dev"),W=l(" to read the documentation"),E=G(),m=i("p"),A=l(`Hello, and welcome to my website! My name is Marty Neal, and I'm a Software Engineer living in Vancouver WA.
+
+`),v=i("p"),H=l(`This website is a platform where I can share my knowledge and experiences with the world. When I'm not working, I love to spend time with my wife and two kids.
+
+`),c=i("p"),V=l(`My hobbies include playing piano, video games, chess. I also love to learn new things, and I'm always looking for new opportunities to grow as a person.
+
+`),u=i("p"),C=l(`Below are some of my projects I've been working on
+
+`),f=i("ul"),y=i("li"),L=l("My daughter is very passionate about helping others all around the world. Check out the site she is building all by herself "),_=i("a"),j=l("here"),q=G(),b=i("li"),B=l("My thesis work involved finite state transducers which you can read about "),k=i("a"),K=l("here"),this.h()},l(e){p=s(e,"H1",{});var o=n(p);x=r(o,"Welcome to SvelteKit"),o.forEach(t),I=U(e),d=s(e,"P",{});var g=n(d);P=r(g,"Visit "),w=s(g,"A",{href:!0});var F=n(w);S=r(F,"kit.svelte.dev"),F.forEach(t),W=r(g," to read the documentation"),g.forEach(t),E=U(e),m=s(e,"P",{});var J=n(m);A=r(J,`Hello, and welcome to my website! My name is Marty Neal, and I'm a Software Engineer living in Vancouver WA.
+
+`),J.forEach(t),v=s(e,"P",{});var O=n(v);H=r(O,`This website is a platform where I can share my knowledge and experiences with the world. When I'm not working, I love to spend time with my wife and two kids.
+
+`),O.forEach(t),c=s(e,"P",{});var Q=n(c);V=r(Q,`My hobbies include playing piano, video games, chess. I also love to learn new things, and I'm always looking for new opportunities to grow as a person.
+
+`),Q.forEach(t),u=s(e,"P",{});var R=n(u);C=r(R,`Below are some of my projects I've been working on
+
+`),R.forEach(t),f=s(e,"UL",{});var M=n(f);y=s(M,"LI",{});var N=n(y);L=r(N,"My daughter is very passionate about helping others all around the world. Check out the site she is building all by herself "),_=s(N,"A",{href:!0});var X=n(_);j=r(X,"here"),X.forEach(t),N.forEach(t),q=U(M),b=s(M,"LI",{});var T=n(b);B=r(T,"My thesis work involved finite state transducers which you can read about "),k=s(T,"A",{href:!0});var Y=n(k);K=r(Y,"here"),Y.forEach(t),T.forEach(t),M.forEach(t),this.h()},h(){z(w,"href","https://kit.svelte.dev"),z(_,"href","/save-world"),z(k,"href","fst")},m(e,o){h(e,p,o),a(p,x),h(e,I,o),h(e,d,o),a(d,P),a(d,w),a(w,S),a(d,W),h(e,E,o),h(e,m,o),a(m,A),h(e,v,o),a(v,H),h(e,c,o),a(c,V),h(e,u,o),a(u,C),h(e,f,o),a(f,y),a(y,L),a(y,_),a(_,j),a(f,q),a(f,b),a(b,B),a(b,k),a(k,K)},p:D,i:D,o:D,d(e){e&&t(p),e&&t(I),e&&t(d),e&&t(E),e&&t(m),e&&t(v),e&&t(c),e&&t(u),e&&t(f)}}}class ie extends ${constructor(p){super(),ee(this,p,null,ae,te,{})}}export{ie as component};
